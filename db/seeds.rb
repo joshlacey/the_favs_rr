6ee89bbca6655e597538bbaa670c5096b52124de
Brigit Rossbach <brigitrossbach@gmail.com>
2017-09-12 15:51:16 -0400

    seeded database + serializers"

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Restaurant.create([
+  {name: "Chipotle", address: "2 Broadway, New York, NY 10004", latitude: 40.70462, longitude: -74.014127 },
+  {name: "Sweetgreen", address: "67 Broadway, New York, NY 10005", latitude: 40.70552, longitude: -74.00842}
+  ])
