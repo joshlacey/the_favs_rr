3696a7ef5232c70d4b89c7ca314e2dc5a62bd33d
joshlacey <williamjoshualacey@gmail.com>
2017-09-14 14:55:19 -0400

    added show and update

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,6 +10,16 @@ class Api::V1::UsersController < ApplicationController
       render json: @user, status: 201
     end
 
+    def show
+      @user = User.find(params[:id])
+      render json: @user, status: 200
+    end
+
+    def update
+      @user = User.find(params[:id])
+      
+    end
+
     private
     def user_params
       params.permit(:body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :create]
+      resources :users, only: [:index, :create, :show, :update]
       resources :restaurants, only: [:index, :create, :destroy]
       resources :dishes, only: [:index, :create, :show]
     end
