2c4dc5d6a81b93cae686400963db4198fc18b0de
Brigit Rossbach <brigitrossbach@gmail.com>
2017-09-15 14:24:24 -0400

    dependent relationship btwn dishes and restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
   has_many :user_restaurants
   has_many :users, through: :user_restaurants
-  has_many :dishes
+  has_many :dishes, :dependent => :destroy
 end
