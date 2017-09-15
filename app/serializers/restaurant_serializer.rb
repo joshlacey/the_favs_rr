ad500c25f567d0d5e6c6cfa36d02262c50f27701
Brigit Rossbach <brigitrossbach@gmail.com>
2017-09-15 12:55:49 -0400

    changed serializer

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,4 +1,4 @@
 class RestaurantSerializer < ActiveModel::Serializer
   attributes :id, :name, :latitude, :longitude, :address, :dishes
-  has_many :dishes
+  has_many :dishes, :dependent => :destroy_all
 end
